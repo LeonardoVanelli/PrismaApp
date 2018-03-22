c47bc3ebaf11a34c26ab4da515c78bfc0b6d1142
leonardo vanelli <Leonardo.Vanelli123@gmail.com>
2018-03-22 17:41:10 -0300

    Add project

---
+++ PrismaApp/PrismaApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PrismaApp.Models;
+
+namespace PrismaApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+
+            Item = new Item
+            {
+                Text = "Item name",
+                Description = "This is an item description."
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopModalAsync();
+        }
+    }
+}
